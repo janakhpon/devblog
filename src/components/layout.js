2d2877246d81e2fe10ac84f63b3d805a2e5b3e93
janakhpon <jnovaxer@gmail.com>
2020-02-17 16:13:31 +0630

    GDEV003 integration step 4

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,48 @@
-import React from 'react';
+/**
+ * Layout component that queries for data
+ * with Gatsby's StaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/static-query/
+ */
 
-import Header from './header';
-import Footer from './footer';
-import '../styles/index.scss';
-import styles from './layout.module.scss';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
+import Header from './header'
 
 const Layout = ({ children }) => {
   return (
-    <div className={styles.container}>
-      <Header />
-      <main className={styles.main}>{children}</main>
-      <Footer />
-    </div>
-  );
-};
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
+          }
+        }
+      `}
+      render={data => (
+        <div style={{ minHeight: '100vh', backgroundColor: '#FFF' }}>
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `0px 1.0875rem 1.45rem`,
+              paddingTop: 100,
+            }}
+          >
+            <main>{children}</main>
+          </div>
+        </div>
+      )}
+    />
+  )
+}
 
-export default Layout;
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
